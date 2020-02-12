70921498afdd448453223ce0c1050dbf390b47de
Quentin Crida <qcrida@gmail.com>
2020-02-12 18:06:19 +0000

    first two Rest routes completed

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,11 +1,35 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/distilleries")
 public class DistilleryController {
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+
+    @GetMapping(value = "/distilleries/{id}")
+    public ResponseEntity getDistillery(@PathVariable Long id){
+        return new ResponseEntity<>(distilleryRepository.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/distilleries/region/{region}")
+    public ResponseEntity<List<Distillery>> getDistilleriesFromRegion(@PathVariable String region){
+        return new ResponseEntity<>(distilleryRepository.findByRegion(region), HttpStatus.OK);
+    }
+
+
 
 
 }
